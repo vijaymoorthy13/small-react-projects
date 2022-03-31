633c9b82466241743835b694fd39fc9abef3f26d
vijaymoorthy13 <85095015+vijaymoorthy13@users.noreply.github.com>
2022-03-31 16:35:08 +0530

    Add files via upload

---
+++ review reactjs/src/food menu filter reactjs/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+   const allCategories =['all',... new Set( items.map((item) => item.category))]  // for unique values 
+                                                         //it takes all same and gives only the same name
+
+function App() {
+   
+  const [menuItems,setMenuItems] = useState(items);
+  const [categories,setCategories] = useState(allCategories);
+
+ const filterItems = (category) =>{
+   
+    if(category === "all"){
+      setMenuItems(items)
+      return;
+    }
+
+      const newItems = items.filter((item) => item.category === category)
+      setMenuItems(newItems)
+      
+ }
+
+  return (
+      <main >
+        <section className='menu section'>
+          <div className='title'>
+            <h2>Menu Items</h2>
+            <div className='underline'></div>
+          </div>
+          <Categories categories={categories} filterItems={filterItems}/>
+          <Menu items={menuItems}/>
+        </section>
+      </main>
+  )
+}
+
+export default App;
